de383eaf516480fe108a2292fa25cb45d196faad
annawaterhouse <annawaterhouse05@gmail.com>
2023-11-28 16:23:25 -0500

    upload form file upload working

--- src/client/features/tickets/Upload.jsx
+++ src/client/features/tickets/Upload.jsx
@@ -8,20 +8,61 @@ export default function Upload() {
   const navigate = useNavigate();
   const [message, setMessage] = useState("");
 
-  const onSubmit = async (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
 
     const newTicket = {
       title: formData.get("title"),
-    }
+      category: formData.get("category"),
+      description: formData.get("description"),
+      time: formData.get("time"),
+      price: +formData.get("price") || 0,
+      address1: formData.get("address1"),
+      address2: formData.get("address2"),
+      city: formData.get("city"),
+      state: formData.get("state"),
+      zip: formData.get("zip"),
+      country: formData.get("country"),
+    };
+
+    console.log(newTicket);
+
+  //   try {
+  //     if (!isValid) {
+  //       setMessage("Required: valid email address");
+  //     } else {
+  //       const response = await createTicket(newTicket).unwrap();
+  //       // console.log(response);
+  //       if (response.message) {
+  //         setMessage(() => response.message);
+  //       }
+  //       if (response.ticket) {
+  //         e.target.reset();
+  //       }
+  //       navigate("/");
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} encType="multipart/form-data">
+      <legend>Upload A Ticket</legend>
       <label>
         Category
-        <input name="title" type="text" placeholder="Enter a title" required />
+        <select name="category" type="text" required>
+            <option value="movie">Movie</option>
+            <option value="concert">Concert</option>
+            <option value="reservation">Reservation</option>
+        </select>
+      </label>
+      <label>
+        Title
+      <input name="title" type="text" placeholder="Enter a title" required />
       </label>
       <label>
         Description
@@ -31,16 +72,24 @@ export default function Upload() {
         Price
         <input name="price" type="number" placeholder="Enter ticket price" />
       </label>
-      
-      <input
-        name="gpa"
-        type="number"
-        inputMode="decimal"
-        min={0.0}
-        max={4.0}
-        step="0.01"
-        placeholder="GPA"
-      />
+      <label>
+        Date & Time 
+        <input
+          type="datetime-local"
+          name="time"
+          min="2023-11-28T00:00"
+          max="2025-01-01T00:00"
+          required
+        />
+      </label>
+      <label>
+        Upload a ticket
+        <input type="file" name="upload" required />
+      </label>
+      <label>
+        Location
+        <input type="text" name="location" placeholder="e.g., City, Country"></input>
+      </label>
       <button type="submit">
         Create Listing
       </button>
