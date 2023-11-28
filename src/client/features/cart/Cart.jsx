f6e62248ea8e54dbf59156bffe2ba4389efcccb9
Nguyen Vo <vophannguyen@gmail.com>
2023-11-28 14:29:25 -0500

    order summary

--- src/client/features/cart/Cart.jsx
+++ src/client/features/cart/Cart.jsx
@@ -1,7 +1,10 @@
+import { useDispatch } from "react-redux";
 import CartItem from "./CartItem";
-import { useGetCartQuery } from "./cartSlice";
+import { addPrice, resetPrice, useGetCartQuery } from "./cartSlice";
 
 export default function Cart() {
+  const dispatch = useDispatch();
+  // dispatch(addPrice(10));
   const { isloading, isError, data } = useGetCartQuery();
   if (isloading) {
     return <h1>Loading....</h1>;
--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import cartReducer from "../features/cart/cartSlice";
 import api from "./api";
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
+    cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
