ce8ae2205412c4b136c31353f16c73188b5f7a12
annawaterhouse <annawaterhouse05@gmail.com>
2023-11-30 13:24:16 -0500

    filtering ticket categories working

--- src/client/features/tickets/Tickets.jsx
+++ src/client/features/tickets/Tickets.jsx
@@ -1,9 +1,5 @@
 import { useGetTicketsQuery, useGetMoviesQuery, useGetConcertsQuery, useGetResQuery } from "./ticketSlice";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import { selectToken } from "../auth/authSlice";
->>>>>>> main
 import { useState } from "react";
 
 //Basic functionality setup
@@ -18,15 +14,11 @@ const TicketCard = ({ ticket }) => {
 /** Main interface for user to interact with their tickets */
 export default function Tickets() {
   const { data: tickets, isLoading, isError } = useGetTicketsQuery();
-<<<<<<< HEAD
   const { data: movies } = useGetMoviesQuery();
   const { data: concerts } = useGetConcertsQuery();
   const { data: res } = useGetResQuery();
   const [ isSorted, setIsSorted ] = useState(false);
   const [ filtered, setFiltered ] = useState(null);
-=======
-  const [newTicket, setNewTicket] = useState(tickets);
->>>>>>> main
   //pagination to be added ?
   //sorting feature - to be added?
   console.log(tickets);
@@ -45,7 +37,6 @@ export default function Tickets() {
     });
     setNewTicket(searchTicket);
   }
-<<<<<<< HEAD
 //need to fix rerendering for every click on the same filter 
 //(click movies once filter, click movies again make sure does not refilter)
   function onSortByMovie() {
@@ -96,24 +87,6 @@ export default function Tickets() {
         ))
         }
       </ul>
-=======
-  // console.log(newTicket);
-  return (
-    <section>
-      {tickets && (
-        <form onSubmit={handleSearch}>
-          <input type="text" placeholder="Search.." name="search" />
-          <button>🔎</button>
-          <h1>Tickets</h1>
-          {isLoading && <span>insert a spinner...</span>}
-          <ul>
-            {newTicket?.map((ticket) => (
-              <TicketCard ticket={ticket} key={ticket.id} />
-            ))}
-          </ul>
-        </form>
-      )}
->>>>>>> main
     </section>
   );
 }
