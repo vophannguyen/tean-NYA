7dd89f3376082166f6ac07ace34b031c6e8f5c46
Nguyen Vo <vophannguyen@gmail.com>
2023-12-01 23:52:06 -0500

    add comment all cart component

--- src/client/features/cart/OrderSummary.jsx
+++ src/client/features/cart/OrderSummary.jsx
@@ -1,20 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-
+/** Show how much user should pay */
 export default function OrderSummary({ data }) {
+  // used Hook to get data from cartSlice, to navigate
   const cart = useSelector((state) => state.cart);
   const navigate = useNavigate();
-  console.log(cart);
+
+  //click checkout will direct to process check out
   function handleCheckout() {
     navigate("/cart/checkout");
   }
   return (
-    <div>
+    <>
       <h1>Order Summary</h1>
       <p>Original Price: {cart.originPrice}</p>
       <p>Sale Tax :{cart.saleTax}</p>
       <p>Total: {cart.total}</p>
       {data && <button onClick={handleCheckout}>Check Out</button>}
-    </div>
+    </>
   );
 }
