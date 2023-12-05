e441ba72a0363f2e90b5bd0bc89e25da751e60e2
annawaterhouse <annawaterhouse05@gmail.com>
2023-12-05 15:00:35 -0500

    styling changes

--- src/client/layout/Root.jsx
+++ src/client/layout/Root.jsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./footer/Footer";
 
-import "./Root.less";
-
 export default function Root() {
   return (
     <div className="root">
