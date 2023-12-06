fcaf7b3d5f9b1b413e0a8e02293a76e1c3198a86
yc530 <yoonachoi530@gmail.com>
2023-12-06 12:34:01 -0500

    finished footer layout and linked

--- src/client/layout/footer/Footer.jsx
+++ src/client/layout/footer/Footer.jsx
@@ -5,51 +5,27 @@ export default function Footer() {
   return (
     <footer>
       <section className="flex">
-      <ul>
-        <li>About</li>
-        <li>
-          <NavLink to="/tickets">Events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/upload">List an event</NavLink>
-        </li>
-        <li>
+        <ul className="about">
+          <li>
             <NavLink to="/about">About Us</NavLink>
-        </li>
-        <li>
+          </li>
+          <li>
             <NavLink to="/faq">FAQ</NavLink>
-        </li>
-      </ul>
-      <ul>
-        <li>Categories</li>
-        <li>
-          <NavLink to="/movies">Movies</NavLink>
-        </li>
-        <li>
-            <NavLink to="/concerts">Concerts</NavLink>
-        </li>
-        <li>
-          <NavLink to="/reservations">Restaurants</NavLink>
-        </li>
-        <li>
-            <NavLink to="/">All Events</NavLink>
-        </li>
-      </ul>
-      </section>
-      <hr />
-      <section className="payment">
-        <p>Payment Methods</p>
-        <ul className="flex">
-          <li>Paypal icon, apple icon, credit cards icon</li>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
         </ul>
       </section>
       <hr />
-      <section className="policy">
-        <ul className="flex">
-          <li><NavLink to="/">Terms & Conditions</NavLink></li>
-          <li>|</li>
-          <li><NavLink to="/">Privacy Policy</NavLink></li>
-          <li>|</li>
+      <section className="flex">
+        <ul className="terms">
+          <li>
+            <NavLink to="/">Terms & Conditions</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Privacy Policy</NavLink>
+          </li>
           <li>Last Chance @ 2024</li>
         </ul>
       </section>
