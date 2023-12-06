565fe06705721f9f690032a5746a08d9e0d1915f
yc530 <yoonachoi530@gmail.com>
2023-12-06 16:17:05 -0500

    changed size of header

--- src/client/features/user/Profile.jsx
+++ src/client/features/user/Profile.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Reservations from "./profile/Reservations";
 import Listings from "./profile/Listings";
 import { useFetchUserAccountQuery } from "./userSlice";
-import "./profile.css"
+import "./profile.less"
+import PastRes from "./profile/PastRes";
 
 export default function Profile() {
   const { data: me, isLoading, error } = useFetchUserAccountQuery();
@@ -14,11 +15,12 @@ export default function Profile() {
     <p>Loading ...</p>
   ) : (
     <main className="account-page">
-      <h1 className="account-header">Welcome, {me?.data.firstName}</h1>
+      <h2 className="account-header">Welcome, {me?.data.firstName}</h2>
       <section className="tab-buttons">
         <button onClick={() => setActiveTab("user")}>User Info</button>
         <button onClick={()=> setActiveTab("reservations")}>Upcoming</button>
         <button onClick={() => setActiveTab("listings")}>Your Listings</button>
+        <button onClick={() => setActiveTab("history")}>Reservation History</button>
       </section>
       <section className="profile-content">
      {activeTab === "user" && (
@@ -29,6 +31,7 @@ export default function Profile() {
      )} 
      {activeTab === "reservations" && <Reservations />}
     {activeTab === "listings" && <Listings />}
+    {activeTab === "history" && <PastRes />}
       </section>
     </main>
   );
