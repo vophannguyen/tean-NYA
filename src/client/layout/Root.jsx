eb7cd1b56977e45f71e89660154facb2b0567225
Nguyen Vo <vophannguyen@gmail.com>
2023-12-06 22:21:38 -0500

    fixed --------

--- src/client/layout/Root.jsx
+++ src/client/layout/Root.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./footer/Footer";
-import "./root.less";
+import "./boxroot.less";
 export default function Root() {
   return (
     <div className="root">
