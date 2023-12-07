c3a7fe84178909f02c7cc5df2bdba5a29be0cd3b
yc530 <yoonachoi530@gmail.com>
2023-12-07 13:04:31 -0500

    added pdf code but changing

--- src/client/features/user/profile/ActiveListings.jsx
+++ src/client/features/user/profile/ActiveListings.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { formatDate } from "../../utils/helpers";
 import { useFetchAllUserItemsQuery } from "../userSlice";
 /**Show sell item of User */
 export default function ActiveListings() {
   const { data: allItems, isLoading, error } = useFetchAllUserItemsQuery();
+  const [selectedItemPdf, setSelectedItemPdf] = useState(null);
   if (error) {
     return <p>Error Fetching Your Reservations</p>;
   }
+  console.log("data", allItems)
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedItemPdf(file);
+  };
   return (
     <section>
       <h2>Active</h2>
@@ -15,6 +22,10 @@ export default function ActiveListings() {
             <li key={reservation.id}>
               {reservation.title}
               <span>Time: {formatDate(reservation.time)}</span>
+              <input type="file" onChange={handleFileChange} />
+              {selectedItemPdf && (
+                <embed src={URL.createObjectURL(selectedItemPdf)} type ="application/pdf" width="100%" height= "600px" />
+              )}
             </li>
           ))}
         </ul>
