6eef6866f04d4374906fe82cc485c4c9ecd85878
Nguyen Vo <vophannguyen@gmail.com>
2023-12-06 22:33:51 -0500

    fixed navbar

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { selectToken } from "../features/auth/authSlice";
 import FlyoutMenu from "./FlyoutMenu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import ProfileIcon from "./ProfileIcon.jsx";
-import "./Navbar.less";
+import "./navbar.less";
 
 export default function Navbar() {
   // const cartItem = useSelector((state) => state.cart.cart);
