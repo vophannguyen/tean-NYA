3c85552ec2dc2c7eda5af3679e25d17c33f77070
yc530 <yoonachoi530@gmail.com>
2023-12-07 13:24:47 -0500

    trying to render pdf files

--- src/client/features/user/profile/ActiveListings.jsx
+++ src/client/features/user/profile/ActiveListings.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
 import { formatDate } from "../../utils/helpers";
 import { useFetchAllUserItemsQuery } from "../userSlice";
-/**Show sell item of User */
+
+/** Show sell item of User */
 export default function ActiveListings() {
-  const { data: allItems, isLoading, error } = useFetchAllUserItemsQuery();
-  const [selectedItemPdf, setSelectedItemPdf] = useState(null);
-  if (error) {
+  const { data: allItems, isLoading, isError } = useFetchAllUserItemsQuery();
+
+  if (isLoading) {
+    return <h1>Loading....</h1>;
+  }
+
+  if (isError) {
     return <p>Error Fetching Your Reservations</p>;
   }
-  console.log("data", allItems)
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedItemPdf(file);
-  };
+
   return (
     <section>
       <h2>Active</h2>
@@ -21,11 +21,21 @@ export default function ActiveListings() {
           {allItems.data.map((reservation) => (
             <li key={reservation.id}>
               {reservation.title}
-              <span>Time: {formatDate(reservation.time)}</span>
-              <input type="file" onChange={handleFileChange} />
-              {selectedItemPdf && (
-                <embed src={URL.createObjectURL(selectedItemPdf)} type ="application/pdf" width="100%" height= "600px" />
+              {reservation.upload.endsWith('.pdf') ? (
+                <embed
+                  src={`http://localhost:3000`/src/server/images`/${reservation.upload}`}
+                  type="application/pdf"
+                  width="100%"
+                  height="600px"
+                />
+              ) : (
+                <img
+                  src={`http://localhost:3000/` + `${reservation.upload}`}
+                  alt="wrong"
+                  style={{ maxWidth: '100%', height: 'auto' }}
+                />
               )}
+              <span>Time: {formatDate(reservation.time)}</span>
             </li>
           ))}
         </ul>
