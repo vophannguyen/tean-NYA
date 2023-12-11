90557a3688ef4b8db424e0061fbc11304f87d79e
yc530 <yoonachoi530@gmail.com>
2023-12-11 15:53:56 -0500

    conditionally rendered scroll buttons to show only if more than 3 reservations

--- src/client/features/user/profile/SoldListings.jsx
+++ src/client/features/user/profile/SoldListings.jsx
@@ -1,7 +1,7 @@
 import { formatDate } from "../../utils/helpers";
 import { useState, useRef, useEffect } from "react";
 import { useGetSoldItemQuery } from "../userSlice";
-import "./profilelisting.less"
+import "./profilelisting.less";
 
 export default function SoldListings() {
   const { data: sold, isLoading, isError } = useGetSoldItemQuery();
@@ -10,17 +10,18 @@ export default function SoldListings() {
   const [scrollEnd, setScrollEnd] = useState(false);
   const scrl = useRef(null);
 
-  
   const slide = (shift) => {
     const container = scrl.current;
     const targetScrollLeft = container.scrollLeft + shift;
 
     container.scrollTo({
       left: targetScrollLeft,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
-  
-    setScrollEnd(targetScrollLeft + container.clientWidth >= container.scrollWidth);
+
+    setScrollEnd(
+      targetScrollLeft + container.clientWidth >= container.scrollWidth
+    );
   };
 
   const scrollCheck = () => {
@@ -64,47 +65,49 @@ export default function SoldListings() {
       <h2>Sold</h2>
       {sold && sold.length > 0 ? (
         <div className="scrollContainer">
-          <button
-          className="scrollButton-left"
-          onClick={() => slide(-1000)}>
-            {'<'}
-          </button>
-        <ul className="horizonalScrollContainer" ref={scrl} onScroll={scrollCheck}>
-          {sold.map((reservation) => (
-            <li key={reservation.id} className="active-card">
-              {reservation.title}{" "}
-              <span> Sold:{formatDate(reservation.createAt)}</span>
-              <button onClick={() => handleViewMoreInfo(reservation.id)}>
-                {selectedItem === reservation.id ? "Back" : "View Ticket"}
-              </button>
-              {selectedItem === reservation.id && (
-                <div>
-                  {reservation.upload.endsWith(".pdf") ? (
-                    <embed
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      type="application/pdf"
-                      width="100%"
-                      height="600px"
-                    />
-                  ) : (
-                    <img
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      alt="wrong"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-        <button
-        className="scrollButton-right"
-        onClick={() => slide(1000)}
-        disabled={scrollEnd}
-        >
-          {'>'}
-        </button>
+          {sold && sold.length > 3 && (
+            <button className="scrollButton-left" onClick={() => slide(-1000)}>
+              {"<"}
+            </button>
+          )}
+          <ul
+            className="horizontalScrollContainer"
+            ref={scrl}
+            onScroll={scrollCheck}
+          >
+            {sold.map((reservation) => (
+              <li key={reservation.id} className="active-card">
+                {reservation.title}{" "}
+                <span> Sold:{formatDate(reservation.createAt)}</span>
+                <button onClick={() => handleViewMoreInfo(reservation.id)}>
+                  {selectedItem === reservation.id ? "Back" : "View Ticket"}
+                </button>
+                {selectedItem === reservation.id && (
+                  <div>
+                    {reservation.upload.endsWith(".pdf") ? (
+                      <embed
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        type="application/pdf"
+                        width="100%"
+                        height="600px"
+                      />
+                    ) : (
+                      <img
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        alt="wrong"
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
+                    )}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+          {sold.length > 3 && (
+            <button className="scrollButton-right" onClick={() => slide(890)}>
+              {">"}
+            </button>
+          )}
         </div>
       ) : (
         <p>No sold items found.</p>
--- src/client/features/user/profile/UpcomingRes.jsx
+++ src/client/features/user/profile/UpcomingRes.jsx
@@ -1,11 +1,58 @@
 import { formatDate } from "../../utils/helpers";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFetchUserReservationHistoryQuery } from "../userSlice";
 
 export default function UpcomingRes() {
   // use RTK to fetch data
   const { data, isLoading, isError } = useFetchUserReservationHistoryQuery();
   const [selectedItem, setSelectedItem] = useState(null);
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollEnd, setScrollEnd] = useState(false);
+  const scrl = useRef(null);
+
+  const slide = (shift) => {
+    const container = scrl.current;
+    const targetScrollLeft = container.scrollLeft + shift;
+
+    container.scrollTo({
+      left: targetScrollLeft,
+      behavior: "smooth",
+    });
+
+    setScrollEnd(
+      targetScrollLeft + container.clientWidth >= container.scrollWidth
+    );
+  };
+
+  const scrollCheck = () => {
+    console.log("Scroll check!");
+    setScrollX(scrl.current.scrollLeft);
+    if (
+      (Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft),
+      +scrl.current.offsetWidth)
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      scrl.current &&
+      scrl?.current?.scrollWidth === scrl?.current?.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  }, [scrl?.current?.scrollWidth, scrl?.current?.offsetWidth]);
+
+  const handleViewMoreInfo = (itemId) => {
+    setSelectedItem((prevSelectedItem) =>
+      prevSelectedItem === itemId ? null : itemId
+    );
+  };
   //use hook
   ///waiting data
   if (isLoading) {
@@ -26,50 +73,63 @@ export default function UpcomingRes() {
     return Date.parse(new Date(item.time)) > Date.now() ? true : false;
   });
 
-  const handleViewMoreInfo = (itemId) => {
-    setSelectedItem((prevSelectedItem) =>
-      prevSelectedItem === itemId ? null : itemId
-    );
-  };
-
   // render
   return (
-    <div>
+    <section className="listing-body">
       <h2>Upcoming</h2>
       {upcoming && upcoming.length > 0 ? (
-        <ul>
-          {upcoming.map((reservation) => (
-            <li key={reservation.id}>
-              {reservation.title}
-              <span> Time: {formatDate(reservation.time)}</span>
-              <button onClick={() => handleViewMoreInfo(reservation.id)}>
-                {selectedItem === reservation.id ? "Back" : "View Ticket"}
-              </button>
-              {selectedItem === reservation.id && (
-                <div>
-                  {reservation.upload.endsWith(".pdf") ? (
-                    <embed
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      type="application/pdf"
-                      width="100%"
-                      height="600px"
-                    />
-                  ) : (
-                    <img
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      alt="wrong"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
-                </div>
-
-              )}
-            </li>
-          ))}
-        </ul>
+        <div className="scrollContainer">
+          {upcoming.length > 3 && (
+            <button className="scrollButton-left" onClick={() => slide(-1000)}>
+              {"<"}
+            </button>
+          )}
+          <ul
+            className="horizontalScrollContainer"
+            ref={scrl}
+            onScroll={scrollCheck}
+          >
+            {upcoming.map((reservation) => (
+              <li key={reservation.id} className="active-card">
+                {reservation.title}
+                <span> Time: {formatDate(reservation.time)}</span>
+                <button onClick={() => handleViewMoreInfo(reservation.id)}>
+                  {selectedItem === reservation.id ? "Back" : "View Ticket"}
+                </button>
+                {selectedItem === reservation.id && (
+                  <div>
+                    {reservation.upload.endsWith(".pdf") ? (
+                      <embed
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        type="application/pdf"
+                        width="100%"
+                        height="600px"
+                      />
+                    ) : (
+                      <img
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        alt="wrong"
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
+                    )}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+          {upcoming.length > 3 && (
+            <button
+              className="scrollButton-right"
+              onClick={() => slide(890)}
+              disabled={scrollEnd}
+            >
+              {">"}
+            </button>
+          )}
+        </div>
       ) : (
         <p>No upcoming reservations found.</p>
       )}
-    </div>
+    </section>
   );
 }
