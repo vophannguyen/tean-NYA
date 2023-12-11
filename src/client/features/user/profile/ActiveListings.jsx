e350084e86c3b6dda34b7dd83a20b681bf83f5a0
yc530 <yoonachoi530@gmail.com>
2023-12-10 22:54:06 -0500

    added scroll buttons, horizontal scroll more tm

--- src/client/features/user/profile/ActiveListings.jsx
+++ src/client/features/user/profile/ActiveListings.jsx
@@ -1,15 +1,57 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { formatDate } from "../../utils/helpers";
-import { useFetchAllUserItemsQuery, useDeleteItemMutation } from "../userSlice";
+import { useFetchAllUserItemsQuery } from "../userSlice";
+import "./profilelisting.less"; 
 
-import styles from "./activelisting.less";
-
-/** Show sell item of User */
 export default function ActiveListings() {
   const { data: allItems, isLoading, isError } = useFetchAllUserItemsQuery();
   const [selectedItem, setSelectedItem] = useState(null);
+  const [scrollX, setScrollX] = useState(0);
+  const [scrollEnd, setScrollEnd] = useState(false);
+  const scrl = useRef(null);
+  
+  const slide = (shift) => {
+    const container = scrl.current;
+    const targetScrollLeft = container.scrollLeft + shift;
 
-  //waiting data
+    container.scrollTo({
+      left: targetScrollLeft,
+      behavior: 'smooth',
+    });
+  
+    setScrollEnd(targetScrollLeft + container.clientWidth >= container.scrollWidth);
+  };
+
+  const scrollCheck = () => {
+    console.log('Scroll check!');
+    setScrollX(scrl.current.scrollLeft);
+    if (
+      Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+      scrl.current.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      scrl.current &&
+      scrl?.current?.scrollWidth === scrl?.current?.offsetWidth
+    ) {
+      setScrollEnd(true);
+    } else {
+      setScrollEnd(false);
+    }
+  }, [scrl?.current?.scrollWidth, scrl?.current?.offsetWidth]);
+
+  const handleViewMoreInfo = (itemId) => {
+    setSelectedItem((prevSelectedItem) =>
+      prevSelectedItem === itemId ? null : itemId
+    );
+  };
+
   if (isLoading) {
     return <h1>Loading....</h1>;
   }
@@ -17,53 +59,58 @@ export default function ActiveListings() {
   if (isError) {
     return <p>Error Fetching Your Reservations</p>;
   }
-  ////end
 
-  //handle view pickture when click
-  const handleViewMoreInfo = (itemId) => {
-    setSelectedItem((prevSelectedItem) =>
-      prevSelectedItem === itemId ? null : itemId
-    );
-  };
-
-  return (
-    <section className="active-card">
-      <h2>Active</h2>
-      {allItems && allItems.data.length > 0 ? (
-        <ul className={styles.horizontalScrollContainer}>
-          {/* <HorizontalScroll> */}
+return (
+  <section className="listing-body">
+    <h2>Active</h2>
+    {allItems && allItems.data.length > 0 ? (
+      <div className="scrollContainer">
+        <button
+          className="scrollButton-left" 
+          onClick={() => slide(-1000)}
+        >
+          {'<'}
+        </button>
+        <ul className="horizontalScrollContainer" ref={scrl} onScroll={scrollCheck}>
           {allItems.data.map((reservation) => (
-            <li key={reservation.id}>
-              {reservation.title}{" "}
-              <span> Time: {formatDate(reservation.time)}</span>
-              <button onClick={() => handleViewMoreInfo(reservation.id)}>
-                {selectedItem === reservation ? "Back" : "View Ticket"}
-              </button>
-              {selectedItem === reservation.id && (
-                <div>
-                  {reservation.upload.endsWith(".pdf") ? (
-                    <embed
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      type="application/pdf"
-                      width="100%"
-                      height="600px"
-                    />
-                  ) : (
-                    <img
-                      src={`http://localhost:10000/${reservation.upload}`}
-                      alt="wrong"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  )}
-                </div>
-              )}
+            <li key={reservation.id} className="active-card">
+                {reservation.title}{" "}
+                <span> Time: {formatDate(reservation.time)}</span>
+                <button onClick={() => handleViewMoreInfo(reservation.id)}>
+                  {selectedItem === reservation.id ? "Back" : "View Ticket"}
+                </button>
+                {selectedItem === reservation.id && (
+                  <div>
+                    {reservation.upload.endsWith(".pdf") ? (
+                      <embed
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        type="application/pdf"
+                        width="100%"
+                        height="300px"
+                      />
+                    ) : (
+                      <img
+                        src={`http://localhost:10000/${reservation.upload}`}
+                        alt="wrong"
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
+                    )}
+                  </div>
+                )}
             </li>
           ))}
-          {/* </HorizontalScroll> */}
         </ul>
-      ) : (
-        <p>No Active Listings</p>
-      )}
-    </section>
-  );
+        <button
+          className="scrollButton-right"
+          onClick={() => slide(890)}
+          disabled={scrollEnd}
+        >
+          {'>'}
+        </button>
+      </div>
+    ) : (
+      <p>No Active Listings</p>
+    )}
+  </section>
+);
 }
