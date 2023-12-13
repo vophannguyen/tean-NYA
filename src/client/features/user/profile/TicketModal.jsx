152986db8e2dae075ec54b211587a270f3742a28
Nguyen Vo <vophannguyen@gmail.com>
2023-12-13 16:04:37 -0500

    fixed ----

--- src/client/features/user/profile/TicketModal.jsx
+++ src/client/features/user/profile/TicketModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./TicketModal.less"; 
+import "./modalticket.less";
 
 const TicketModal = ({ src, onClose }) => {
   return (
